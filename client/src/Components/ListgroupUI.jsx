3d47e2e23e95eda937654e68715f0056fa0b12a3
jzf21 <jozefneelamkavil2003@gmail.com>
2023-01-17 21:17:00 +0530

    new

--- client/src/Components/ListgroupUI.jsx
+++ client/src/Components/ListgroupUI.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Image from "next/image";
 export const ListgroupUI = ({ items }) => {
   return (
-    <div class="flex justify-center flex-col bg-white py-5 rounded-t-xl">
+    <div class="flex justify-center flex-col bg-black py-5">
       <div className="title ">
-        <h1 className="text-2xl text-black p-2 font-semibold">Recent Expenses</h1>
+        <h1 className="text-2xl text-white p-2 px-5 font-semibold">Recent Expenses</h1>
       </div>
-      <ul class="bg-white rounded-lg border border-gray-200 w-full text-gray-900">
+      <ul class="bg-black rounded-lg  w-full text-slate-100">
         {items.map((item) => (
-          <li class="px-6 py-2 border-b border-gray-200 w-full">
+          <li class="px-4 py-2 w-full">
             <div class="flex flex-row">
               <div className="icon_name basis-1/2">
                 <div className="flex flex-row gap-3">
@@ -20,18 +20,18 @@ export const ListgroupUI = ({ items }) => {
                   />
                   <div className="flex flex-col">
                     <h1 className="text-lg font-semibold">{item.name}</h1>
-                    <p className="text-sm text-slate-700">
+                    <p className="text-sm text-slate-300">
                       {new Date(item.date * 1000).toUTCString().split(" ").join(" ").split(" ").slice(1, 4).join(" ") }
                     </p>
                   </div>
                 </div>
               </div>
               <div className="basis-1/2">
-                <div className="flex flex-col  justify-end">
-                  <p className="text-xl font-semibold">₹{item.budget}</p>
-                  <p className="text-sm">
+                <div className="flex flex-col flex-end justify-end">
+                  <p className="text-xl font-semibold flex-end text-right">-₹{item.budget}</p>
+                  {/* <p className="text-sm">
                     {(item.budget / 1000) * 100}% of monthly budget spent
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </div>
